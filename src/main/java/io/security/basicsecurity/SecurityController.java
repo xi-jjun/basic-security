885e4ebb8b27bf1daceb2243010ec5eb09a3259c
xi_jjun <rlawowns97@naver.com>
2022-03-13 15:46:53 +0900

    refactor: add loginPage get mapping for routing user defined login page

--- src/main/java/io/security/basicsecurity/SecurityController.java
+++ src/main/java/io/security/basicsecurity/SecurityController.java
@@ -8,12 +8,15 @@ public class SecurityController {
 	/**
 	 * 정상적으로 서버에 접근할 수 있지만 '누구라도' 접근이 가능하다.
 	 * => 보완에 취약한 구조.
-	 *
 	 * 현재 시스템을 보완이 적용된 시스템으로 작업을 할 것이다. => SecurityConfig 로 사용자 정의 보안 class 생성
-	 * @return
 	 */
 	@GetMapping("/")
 	public String home() {
 		return "home";
+	}
+
+	@GetMapping("/loginPage")
+	public String loginPage() {
+		return "login page";
 	}
 }
